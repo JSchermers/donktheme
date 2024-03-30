4cc8e9f83ab5f6f54339f5f69077ff7ec09a3569
Joost S <joost.schermers@protonmail.com>
2024-03-30 15:46:24 +0100

    added footer

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,12 +12,36 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<!-- do do -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer">
+	<div class="header-footer-row">
+		<div class="content-width">
+			<img src="http://donk.local/wp-content/themes/funfun/assets/images/logo.png" alt="donk logo" class="logo">
+			<div class="top-sponsored">
+				<h2>Sponsors selectie</h2>
+				<div class="grid">
+					<div>Sponsor1</div>
+					<div>Sponsor2</div>
+					<div>Sponsor3</div>
+				</div>
+			</div>
+			<div class="sponsors">
+				<h2>Sponsors club</h2>
+				<div class="grid">
+					<div>club1</div>
+					<div>club2</div>
+					<div>club3</div>
+					<div>club4</div>
+					<div>club5</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- do do -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
--- homepage.php
+++ homepage.php
@@ -7,7 +7,7 @@ get_header();
 ?>
 
 <main id="primary" class="site-main">
-	<div class="header-row">
+	<div class="header-footer-row">
 		<div class="content-width grid">
 			<div class="news-head">
 				<?php
@@ -88,37 +88,7 @@ get_header();
 	</div>
 
 </main><!-- #main -->
-<?php
-// the query.
 
-// READ
-$args = array(
-	'post_type' => 'post',
-	'post_status' => 'publish'
-);
-$the_query = new WP_Query($args); ?>
-
-<?php if ($the_query->have_posts()) : ?>
-	<!-- pagination here -->
-
-	<!-- the loop -->
-	<?php
-	while ($the_query->have_posts()) :
-		$the_query->the_post();
-
-		the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-
-	endwhile; ?>
-	<!-- end of the loop -->
-	
-	<?php posts_nav_link(); ?>
-	<!-- pagination here -->
-
-	<?php wp_reset_postdata(); ?>
-
-<?php else : ?>
-	<p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
-<?php endif; ?>
 
 <?php
 get_footer();
