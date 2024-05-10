955091b83311b81365af74e1a9c2f4837aa1b3d2
kabedesign <koen@kabedesign.nl>
2024-05-10 21:44:22 +0200

    Team overzicht - archive template
    Team overzicht

---
+++ archive-team.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package DonkTest
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+	<div class="content-width grid">
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<h1>Teams</h1>
+			</header><!-- .page-header -->
+
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				echo'<div class=team>';
+					funfun_post_thumbnail();
+					the_title('<h3 class="page-title">', '</h3>');
+				echo'</div>';
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+	</div>
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
